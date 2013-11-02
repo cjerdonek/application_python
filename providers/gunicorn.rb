c439253af3ae16d996eab203aa70bd28b0ea1b63
Chris Jerdonek <chris.jerdonek@gmail.com>
2013-11-02 13:06:48 -0700

    Run gunicorn from the django virtualenv if django is there.
    This lets us follow the following guidance on the Django web site:
    
    "If you are using Django 1.4 or newer, it’s highly recommended to simply
    run your application with the WSGI interface using the gunicorn command
    as described above."
    
    (from https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/gunicorn/#using-gunicorn-s-django-integration )

--- providers/gunicorn.rb
+++ providers/gunicorn.rb
@@ -75,12 +75,15 @@ action :before_deploy do
     if new_resource.environment
       environment new_resource.environment
     end
+    django_resource = get_django_resource
     if new_resource.app_module == :django
-      django_resource = new_resource.application.sub_resources.select{|res| res.type == :django}.first
       raise "No Django deployment resource found" unless django_resource
       base_command = "#{::File.join(django_resource.virtualenv, "bin", "python")} manage.py run_gunicorn"
     else
-      gunicorn_command = new_resource.virtualenv.nil? ? "gunicorn" : "#{::File.join(new_resource.virtualenv, "bin", "gunicorn")}"
+      # Use the django virtualenv if it is there because that is where
+      # gunicorn gets installed above.
+      virtualenv = django_resource ? django_resource.virtualenv : new_resource.virtualenv
+      gunicorn_command = virtualenv.nil? ? "gunicorn" : "#{::File.join(virtualenv, "bin", "gunicorn")}"
       base_command = "#{gunicorn_command} #{new_resource.app_module}"
     end
     template_info = new_resource.supervisor_template
@@ -92,6 +95,7 @@ action :before_deploy do
     end
     supervisord_logfile new_resource.supervisor_logfile
     command "#{base_command} -c #{new_resource.application.path}/shared/gunicorn_config.py"
+    # The directory to which supervisor should temporarily chdir before exec'ing the child.
     directory new_resource.directory.nil? ? ::File.join(new_resource.path, "current") : new_resource.directory
     autostart new_resource.autostart
     user new_resource.owner
